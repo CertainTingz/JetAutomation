970ec82bb2e163577973e89471ebe11d6c3cf25d
CertainTingz <lyndonsimango@gmail.com>
2025-12-20 12:29:34 +0100

    Added TestNG dependancy in POM, removed IDE files from Github and added Setup and Teardown methods in BaseTest Class

--- src/test/java/base/BaseTest.java
+++ src/test/java/base/BaseTest.java
@@ -1,5 +1,38 @@
 package base;
 
+import com.microsoft.playwright.Browser;
+import com.microsoft.playwright.BrowserType;
+import com.microsoft.playwright.Playwright;
+import com.microsoft.playwright.Page;
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.BeforeMethod;
+
+
 public class BaseTest {
+
+    protected Playwright playwright;
+    protected Browser browser;
+    protected Page page;
+
+
+    @BeforeMethod
+    public void setUp() {
+
+        // Playwright Setup
+        playwright = Playwright.create();
+        browser = playwright.chromium().launch(new BrowserType.LaunchOptions().setHeadless(false).setChannel("msedge"));
+
+        page = browser.newPage();
+
+    }
+
+
+    @AfterMethod
+    public void tearDown() {
+        if(browser != null) browser.close();
+        if(playwright != null) playwright.close();
+    }
+
+
 
 }
