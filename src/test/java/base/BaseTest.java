81e9cad5538d0921c4c944d38a5c2d36dda1271d
CertainTingz <lyndonsimango@gmail.com>
2025-12-23 20:51:55 +0100

    Added Reporting and Screenshot functionality.

--- src/test/java/base/BaseTest.java
+++ src/test/java/base/BaseTest.java
@@ -1,11 +1,18 @@
 package base;
 
+import com.aventstack.extentreports.ExtentReports;
+import com.aventstack.extentreports.ExtentTest;
 import com.microsoft.playwright.Browser;
 import com.microsoft.playwright.BrowserType;
 import com.microsoft.playwright.Playwright;
 import com.microsoft.playwright.Page;
+import org.testng.ITestResult;
 import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeMethod;
+import utilities.ExtentManager;
+import utilities.ScreenshotUtility;
+
+import java.lang.reflect.Method;
 
 
 public class BaseTest {
@@ -13,10 +20,17 @@ public class BaseTest {
     protected Playwright playwright;
     protected Browser browser;
     protected Page page;
+    protected ExtentReports extent;
+    protected ExtentTest test;
 
 
     @BeforeMethod
-    public void setUp() {
+    public void setUp(Method method) {
+
+        //Reporting
+        extent = ExtentManager.getInstance();
+        test = extent.createTest(method.getName());
+
 
         // Playwright Setup
         playwright = Playwright.create();
@@ -27,11 +41,28 @@ public class BaseTest {
     }
 
     @AfterMethod
-  public void tearDown() {
+  public void tearDown(ITestResult testResult) {
+
+
+        //Generate Report with Screenshots.
+
+        if(testResult.getStatus() == ITestResult.FAILURE) {
+            test.fail(testResult.getThrowable());
+
+            // Capturing and Attaching Screenshot
+            String screenshotPath = ScreenshotUtility.takeScreenshot(page,testResult.getName());
+            System.out.println("Path Here Please: " + screenshotPath);
+
+            test.addScreenCaptureFromPath(screenshotPath, "Screenshot");
+
+        }else if(testResult.getStatus() == ITestResult.SUCCESS) {
+            test.pass("Test Passed");
+        } else {
+            test.skip("Test Skipped");
+        }
+        extent.flush();
+
     if(browser != null) browser.close();
     if(playwright != null) playwright.close();
     }
-
-
-
 }
